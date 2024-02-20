b83195be6842950d10a30ca8f5c3f4c0bd3ca17a
Ibrahim <ibz_374@hotmail.com>
2024-02-20 23:20:38 +0000

    Task 4 complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -51,3 +51,42 @@ describe('endpoints', () => {
         })
     })
 })
+
+describe('/api/articles/:article_id', () => {
+    test('GET: 200 Responds with an article object', () => {
+        return request(app)
+        .get('/api/article/1')
+        .expect(200)
+        .then((response) => {
+            const article = response.body
+            expect(article).toMatchObject({
+                article_id: 1,
+                title: 'Living in the shadow of a great man',
+                topic: 'mitch',
+                author: 'butter_bridge',
+                body: 'I find this existence challenging',
+                created_at: '2020-07-09T20:11:00.000Z',
+                votes: 100,
+                article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+              })
+        })
+    })
+    test('GET:404 sends an appropriate status and error message when given a valid but non-existent id', () => {
+        return request(app)
+        .get('/api/article/999')
+        .expect(404)
+        .then((response) => {
+            expect(response.body.status).toBe(404)
+            expect(response.body.msg).toBe('Article not found')
+        })
+    })
+    test('GET:400 sends an appropriate status and error message when given an invalid id', () => {
+        return request(app)
+        .get('/api/article/invalid')
+        .expect(400)
+        .then((response) => {
+            expect(response.body.status).toBe(400)
+            expect(response.body.msg).toBe('Bad request')
+        })
+    })
+})
--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const app = express();
 const { getTopics, getEndpoints } = require('./controller/topics.controller')
+const { getArticleId } = require('./controller/articles.controller')
+
 
 app.get("/api/topics", getTopics)
 
 app.get("/api", getEndpoints)
 
+app.get("/api/article/:article_id", getArticleId)
+
+
+app.use((err, req, res, next) => {
+    if (err.code === '23502' || err.code === '22P02') {
+        res.status(400).send({ status: 400, msg: 'Bad request'})
+    }
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({ msg: err.msg})
+    }
+    next(err)
+})
+
 app.all("/api/*", (req, res, next) => {
     res.status(404).send({ msg: 'Unable to find'})
 })
-
-
+app.use((err, req, res, next) => {
+    res.status(500).send({ msg: 'Internal Server Error'})
+  })
 
 module.exports = app;
